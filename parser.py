af8ed152faec55bdfa0ef6040ae838897ad5fd0a
tadzio8425 <js.ortegar1@uniandes.edu.co>
2022-02-16 22:00:20 -0500

    Avance en definición de comandos

--- parser.py
+++ parser.py
@@ -7,7 +7,11 @@ class Parser:
     def __init__(self):
         os.chdir(sys.path[0])
         self.commands = ""
-        self.blocks=[]
+        self.blocks = []
+        self.checked_blocks = []
+
+        self.def_variables = {}
+        self.def_functions = {}
 
     def set_commands(self, file_name):
         with open(file_name) as command_file:
@@ -17,6 +21,7 @@ class Parser:
         self.delete_tabs()
         self.check_parenthesis()
         self.detect_blocks()
+        self.evaluate_blocks()
 
     def delete_tabs(self):
         self.commands = self.commands.replace("\n","")
@@ -41,6 +46,7 @@ class Parser:
 
         return len(parenthesis_stack) == 0        
 
+
     def detect_blocks(self):
         parenthesis_stack = []
         count = 0
@@ -59,13 +65,56 @@ class Parser:
             count += 1
             
 
-        print(self.blocks)
-
-
         
 
 
+    def evaluate_blocks(self):
+
+        for block in self.blocks:
+            checked_block = self.evaluate_production(block)
+            self.checked_blocks.append(checked_block)
+
+
+    def evaluate_production(self, block) -> tuple:
+
+        block_definition = (False, None) #Tupla que determina si el bloque es válido [0] y el grupo al que pertenece [1]
+
+        #Command evaluation
+        block = block[1:len(block)-1]
+        instruction = block.split(" ")
+
+        if "defvar" in block and len(instruction) == 3:   #Regla de producción -- Definición de variables
+            if instruction[0] == "defvar" and type(instruction[1]) == str and self.isNumber(instruction[2]):
+                block_definition = (True, "DEFVAR")
+                self.def_variables[instruction[1]] = instruction[2]
+
+        elif "=" in block and len(instruction) == 3:
+            if instruction[0] == "=" and type(instruction[1]) == str and self.isNumber(instruction[2]):
+                if self.isVariable(instruction[1]):
+                    block_definition = (True, "ASSINGVAR")
+
+        elif "move" in block and len(instruction) == 2:
+            if instruction[0] == "move" and (self.isNumber(instruction[1]) or self.isVariable(instruction[1])):
+                block_definition = (True, "MOVE")
+            
+        return block_definition    
+
+    def isNumber(self, number) -> bool:
+        check = True
+        try:
+            float(number)   
+        except:
+            check = False; 
+        return check
+
+
+    def isVariable(self, name)->bool:
+        check = False
+        if name in self.def_variables.keys():
+            check = True
+        return check
 
 parser = Parser()
 parser.set_commands("invalidCommands.txt")
 parser.parse()
+print(parser.checked_blocks)
